b56da36155f84831be9a1487f9a598895d3d9112
thecheerfuldev <mark@markhendriks.nl>
2022-04-02 08:24:34 +0200

    Cleaned up test and updated spring boot to 2.6.6

--- src/test/java/nl/thecheerfuldev/exampletestingwithspring/api/PersonControllerRestAssuredIntegrationTest.java
+++ src/test/java/nl/thecheerfuldev/exampletestingwithspring/api/PersonControllerRestAssuredIntegrationTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
